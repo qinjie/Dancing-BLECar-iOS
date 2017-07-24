3a116cd7f286b27cd4530895bc2a710046ebc240
Anh Tuan <linhhoang830@gmail.com>
2017-07-24 13:32:10 +0800

    Init project

---
+++ Mouse_Dance/ControlViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ControlViewController.swift
+//  Mouse_Dance
+//
+//  Created by Anh Tuan on 7/24/17.
+//  Copyright © 2017 Anh Tuan. All rights reserved.
+//
+
+import UIKit
+
+class ControlViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
